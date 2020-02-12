90c4b75d2baee86e24cfcc3720b411aaa7bdd3d0
0118Add <60157610+0118Add@users.noreply.github.com>
2020-02-13 05:32:43 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,9 +8,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-#git clone https://github.com/brokeld/luci-app-koolproxyR.git package/luci-app-koolproxyR
-
-#git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-mini.git package/luci-app-unblockneteasemusic-mini
-
-#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/8688Add/luci-app-vssr-jerrykuku.git package/luci-app-vssr
 
